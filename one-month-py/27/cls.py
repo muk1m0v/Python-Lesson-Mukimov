f5886884faab3f4ae8178c031bde88822c251782
muk1m0v <mukimov.shop@gmail.com>
2026-07-10 16:10:44 +0500

    Python new lesson

---
+++ one-month-py/27/cls.py
@@ -0,0 +1,33 @@
+# n = input('-------------\n1 - Add in list\n2 - Delete For ID\n0 - exit')
+# data_db = []
+
+# match n:
+#     case '1':
+#         info = input('Enter the text for list:\n>>> ')
+#         data_db.append(info)
+#         print(f'{info} - add for data - list')
+#     case '2':
+#         delete = int(input('Enter the index for delete:\n>>> '))
+#         data_db.pop(delete)
+#     case '3':
+#         print(data_db)
+#     case '0':
+#         break
+
+# lict = []
+
+# for i in range(1,11):
+#     lict.append(i)
+# for i in lict:
+#     if i == len(lict):
+#         print(i)
+#     else:
+#         print(i, end=',')
+
+# colors = input('Input the colors:\n>>> ').split()
+
+# i = len(colors)
+# while i>0:
+#     print(colors[i-1],end=' ')
+#     i-=1
+
